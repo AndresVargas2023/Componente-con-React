e7a1808895dcfb9da99df4ff6d152b077cbf423f
Andres Vargas <av6135@gmail.com>
2023-12-15 01:50:23 -0300

    Apoyalo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import PersonCard from './Component/PersonCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="person-card">
+      <PersonCard
+        firstName="John"
+        lastName="Jane"
+        age={45}
+        hairColor="Black"
+      />
+      <PersonCard
+        firstName="Smith"
+        lastName="John"
+        age={88}
+        hairColor="Brown"
+      />
+      <PersonCard
+        firstName="Fillmore"
+        lastName="Millard"
+        age={50}
+        hairColor="Brown"
+      />
+      <PersonCard
+        firstName="Smith"
+        lastName="María"
+        age={62}
+        hairColor="Brown"
+      />
     </div>
   );
 }
---
+++ src/Component/MiComponente.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+    class MiComponente extends Component{
+        render(){
+            return(                
+            <div> 
+                Hello Andres
+            </div>
+        
+            )
+        }
+    }
+
+    export default MiComponente;
+    
